cd9b542be96c65af0baac690498fd915e521ea87
miracledev03 <miracledev0303@gmail.com>
2022-05-02 17:13:17 -0300

    Assignment 4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
 import ExpensesList from './ExpensesList';
+import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -12,7 +12,7 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
@@ -23,7 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpensesList items={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
