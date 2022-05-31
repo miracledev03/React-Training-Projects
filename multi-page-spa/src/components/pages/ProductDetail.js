a890dcadf8726ee4324aac060d7b708483f49413
miracledev03 <miracledev0303@gmail.com>
2022-05-31 10:33:22 -0300

    multi-page-spa

---
+++ multi-page-spa/src/components/pages/ProductDetail.js
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+
+const ProductDetail = () => {
+  const params = useParams();
+
+  console.log(params.productId);
+
+  return (
+    <section>
+      <h1>Product Detail Page.</h1>
+
+      <p>
+        {params.productId}
+      </p>
+    </section>
+  );
+}
+
+export default ProductDetail;
