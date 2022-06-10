18345210718c6734d138f0befaf459ac75501819
miracledev03 <miracledev0303@gmail.com>
2022-06-10 09:23:30 -0300

    replace-redux

---
+++ replace-redux/src/containers/Products.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import { useContext } from 'react';
+import { useStore } from '../hooks-store/store';
+// import { useSelector } from 'react-redux';
+
+import ProductItem from '../components/Products/ProductItem';
+// import { ProductsContext } from '../context/products-context';
+import './Products.css';
+
+const Products = props => {
+  // const productList = useSelector(state => state.shop.products);
+  // const productList = useContext(ProductsContext).products;
+  const state = useStore()[0];
+
+  return (
+    <ul className="products-list">
+      {state.products.map(prod => (
+        <ProductItem
+          key={prod.id}
+          id={prod.id}
+          title={prod.title}
+          description={prod.description}
+          isFav={prod.isFavorite}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Products;
